8cf0c47a13df5fe3d2f99a779b4eefb5dd1d0abd
jobailla <jobailla@student.42.fr>
2018-11-22 16:27:10 +0100

    update

--- capture_lissajous.py
+++ capture_lissajous.py
@@ -4,18 +4,21 @@ import sys
 import time
 import threading
 from picamera import PiCamera
+from fractions import Fraction
 
 camera = PiCamera()
 camera.resolution = (800, 600)
+camera.framerate = 60
 camera.start_preview()
 
-f1 = 90.0
+
+f1 = 120.0
 f2 = 0.0        # f2 = n+1/n * f1
-a = 1.0
+a = 2.0
 
 volume = 0.2    # range [0.0, 1.0]
 fs = 44100
-duration = 2.5
+duration = 10.0
 samples_1 = 0.0
 samples_2 = 0.0
 file_name = 0
@@ -42,7 +45,7 @@ def start_threads():
     cap_laser = capture_thread()
     play_freq_1.start()
     play_freq_2.start()
-    time.sleep(0.4)
+    time.sleep(0.5)
     cap_laser.start()
     cap_laser.join()
     play_freq_1.join()
@@ -59,6 +62,7 @@ def stop_stream():
 
 while f1 <= 300.0:
 	f2 = (a + 1.0) / a * f1
+	camera.shutter_speed = int(1000000 / f2 - f1)
         samples_1 = (np.sin(2 * np.pi * np.arange(fs * duration) * f1 / fs)).astype(np.float32)
         samples_2 = (np.sin(2 * np.pi * np.arange(fs * duration) * f2 / fs)).astype(np.float32)
         file_name = "dataset:" + time.strftime("%d-%m-%Y") + "_hz:" + str(round(f1, 1)) + "_" + str(round(f2,3)) + "_lsj:" + str(int(a + 1)) + "." + str(int(a)) + ".jpg"
@@ -67,6 +71,6 @@ while f1 <= 300.0:
     	sys.stdout.flush()
         a += 1
 	if a == 10.0:
-	    f1 += 1.0
-            a = 1
+	    f1 += 10.0
+	    a = 1
 stop_stream()
